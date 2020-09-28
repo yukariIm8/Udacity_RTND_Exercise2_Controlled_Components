bf19b8dc400e1c0696f6730900d381bbc53c33c0
yukariIm8 <yukariim8@gmail.com>
2020-09-28 08:47:28 +0000

    feat: stateless functional component to handle item's deletion.

---
+++ src/DeleteLastItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* Stateless Functional Component */
+
+const DeleteLastItem = props => {
+
+  const handleDeleteLastItem = event => {
+  	props.onDeleteLastItem();
+  };
+  
+  return (
+        <button onClick={handleDeleteLastItem} disabled={props.buttonDisabled}>
+          Delete Last Item
+        </button>
+  );
+};
+
+
+
+DeleteLastItem.propTypes = {
+	onDeleteLastItem: PropTypes.func.isRequired,
+  	buttonDisabled: PropTypes.func.isRequired
+};
+
+export default DeleteLastItem;
