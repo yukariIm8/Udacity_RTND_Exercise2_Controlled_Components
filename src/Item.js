5d3b44b5eaf9fb83fb90ac77b00a29a9ffb4dd41
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-09-28 02:00:23 -0700

    style: fix indentation.

--- src/Item.js
+++ src/Item.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 /* Stateless functional component. */
 
 const Item = props => {
-	return <li>{props.item}</li>;
+  return <li>{props.item}</li>;
 };
 
 Item.propTypes = {
-	item: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
 };
 
 export default Item;
