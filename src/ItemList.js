532a1b0ded8029971e8f83a429a587b523d4d2f0
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-09-28 02:03:03 -0700

    style: src/ItemList.js

--- src/ItemList.js
+++ src/ItemList.js
@@ -5,17 +5,17 @@ import PropTypes from 'prop-types';
 /* Stateless functional component. */
 
 const ItemList = props => {
-	return (
-    	<div>
-      		<p className='items'>Items</p>
-      		<ol className='item'>{props.items.map((item, index) => 
-    			<Item key={index} item={item} />)}</ol>
-      	</div>
-    );
+  return (
+    <div>
+      <p className='items'>Items</p>
+      <ol className='item'>{props.items.map((item, index) => 
+        <Item key={index} item={item} />)}</ol>
+    </div>
+  );
 };
 
 ItemList.propTypes = {
-	items: PropTypes.array.isRequired,
+  items: PropTypes.array.isRequired,
 };
 
 export default ItemList;
